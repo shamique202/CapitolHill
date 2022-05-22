847befbc1f9f786c262137174eabc85cc3bcef09
shamique202 <shamique.e@gmail.com>
2022-05-21 21:04:33 -0400

    commented out code and console.log my error

--- config/auth.js
+++ config/auth.js
@@ -10,6 +10,8 @@ module.exports = function (req, res, next) {
     // Check if token is valid and not expired
     jwt.verify(token, SECRET, function (err, decoded) {
       if (err) {
+        // console.log('error in jwt verify')
+        // res.status(400).json({err})
         next(err);
       } else {
         // It's a valid token, so add user to req
