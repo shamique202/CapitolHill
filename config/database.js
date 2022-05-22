290e87d5cb822274fb226092ba4a5b250053c34e
shamique202 <shamique.e@gmail.com>
2022-05-22 14:14:02 -0400

    linked my databased

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.DATABASE_URL, {
+mongoose.connect(process.env.DATABASE_URL, { // mongodb://localhost:27017/testagramV2 
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
