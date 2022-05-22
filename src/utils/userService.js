e1143f582effe40badd7252e3285fc9dfff847e0
shamique202 <shamique.e@gmail.com>
2022-05-22 14:49:04 -0400

    implement JWT header and authorizations

--- src/utils/userService.js
+++ src/utils/userService.js
@@ -3,7 +3,7 @@ import tokenService from "./tokenService";
 const BASE_URL = "/api/users/";
 
 // NOTE THIS IS configured to send of a multi/part form request
-// aka photo
+// aka photo <--- 
 function signup(user) {
   return (
     fetch(BASE_URL + "signup", {
@@ -12,6 +12,7 @@ function signup(user) {
     })
       .then((res) => {
         if (res.ok) return res.json();
+        console.log("if an eror occurs, check the server in terminal")
         // Probably a duplicate email
         throw new Error("Email already taken!");
       })
@@ -24,15 +25,15 @@ function signup(user) {
   //.then((token) => token.token);
 }
 
-function getUser() {
+function getUser() { // <--- identifying the user but they must be logged in 
   return tokenService.getUserFromToken();
 }
 
-function logout() {
+function logout() { // <--- used now able to logout
   tokenService.removeToken();
 }
 
-function login(creds) {
+function login(creds) { // <--- user identity and password authentication
   return fetch(BASE_URL + "login", {
     method: "POST",
     headers: new Headers({ "Content-Type": "application/json" }),
@@ -45,12 +46,24 @@ function login(creds) {
     })
     .then(({ token }) => tokenService.setToken(token));
 }
+function getProfile(username) {
+  return fetch(BASE_URL + username, {
+    headers: { 
+      Authorization: "Bearer" + tokenService.getToken(), },
+}).then((res) => {
+  if(res.ok) return res.json();
+  if(res.status ===404) throw new Error("User not found!");
+  throw new Error("Bad credentials"); // catch block
+}
+)
+}
 
 const userService = {
   signup,
   logout,
   login,
   getUser,
+  getProfile
 };
 
 export default userService;
