e1af51b99a41382781bd58005cf31a2bd6be9054
shamique202 <shamique.e@gmail.com>
2022-05-23 17:52:07 -0400

    login page functions and signup form done

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -2,18 +2,93 @@ import React, { useState } from "react";
 import "./LoginPage.css";
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
 import userService from "../../utils/userService";
+import {useNavigate, Link } from "react-router-dom";
+import {
+  Button,
+  Form,
+  Grid,
+  Image,
+  Message,
+  Segment,
+} from "semantic-ui-react";
 
 export default function LoginPage(props) {
+  const [error, setError] = useState("");
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  function handleChange(e) {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      await userService.login(state);
+      props.handleSignUpOrLogin();
+      navigate("/");
+    } catch (err) {
+      setError(err.message);
+    }
+  }
   return (
     <>
-      <h1>Setup Login Page</h1>
-      <ul>
-        <li>Read the Login Model, You can change it to fit your needs</li>
-        <li>
-          Make sure you read the Login func in the User Controller, to know how
-          it is setup to find the user!
-        </li>
-      </ul>
+   <Grid
+        textAlign="center"
+        style={{ height: "100vh" }}
+        verticalAlign="middle"
+      >
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Image src="https://i.imgur.com/12PBKfs.jpg" size="small" centered/>
+          <Message
+              attached
+              header='Welcome back!'
+              content='Welcome to Capitol Hill, a fashion blog for displaying your creativity. Signup to view our content!'/> 
+          <Form autoComplete="off" onSubmit={handleSubmit}>
+            <Segment stacked>
+              <Form.Input
+                fluid 
+                icon="user"
+                iconPosition="left"
+                type="email"
+                name="email"
+                placeholder="Email"
+                value={state.email}
+                onChange={handleChange}
+                required />
+              <Form.Input
+                fluid
+                icon="lock"
+                iconPosition="left"
+                name="password"
+                type="password"
+                placeholder="Password"
+                value={state.password}
+                onChange={handleChange}
+                required />
+              <Button
+                color="blue"
+                fluid
+                size="large"
+                type="submit"
+                className="btn">
+                Login
+              </Button>
+            </Segment>
+          </Form>
+          <Message>
+            Are you new? <Link to="/signup">Sign Up Here!</Link>
+          </Message>
+          {error ? <ErrorMessage error={error} /> : null} </Grid.Column>
+      </Grid>
     </>
   );
 }
