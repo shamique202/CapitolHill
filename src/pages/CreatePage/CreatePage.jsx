d8b227cf7dfa03b2d098cbab53d1b954afc966f7
shamique202 <shamique.e@gmail.com>
2022-05-23 19:37:32 -0400

    added handleaddpostform and its function

---
+++ src/pages/CreatePage/CreatePage.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import Loader from "../../components/Loader/Loader";
+import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
+import AddPostForm from "../../components/AddPostForm/AddPostForm";
+import { Grid, Button, Container, Header, Icon } from "semantic-ui-react";
+import * as mypostApi from '../../utils/mypostApi';
+import { Link } from 'react-router-dom';
+
+export default function CreateForm(props) {
+    const [posts, setPosts] = useState([])
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+
+  async function handleAddPostForm(post) {
+        console.log(post)
+        try {
+            setLoading(true);
+            const data = await mypostApi.create(post);
+            console.log(data, "the server responds back now due to the addpostform (handle)");
+            setPosts([data.post, ...posts]);
+            setLoading(false);
+        } catch (err) {
+            setError(err.message);
+            console.log(err)
+        }
+    }
+return (
+        <Container>
+            <Grid centered>
+                <Grid.Row>
+                    <Grid.Column style={{maxWidth: 450}}>
+                        <Header
+                            as='h1'
+                            content='Make your post!'
+                            style={{
+                                fontSize: '4em',
+                                fontWeight: 'bold',
+                                marginBottom: '1em',
+                                marginTop: '0', }} />
+                        <AddPostForm handleAddPostForm={handleAddPostForm} />
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+    </Container>   
+    );
+}
