abbbfb1c935a28b855f11f6e1547dcd8b5b90bc3
shamique202 <shamique.e@gmail.com>
2022-05-23 19:38:31 -0400

    edited code and added more comments

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -13,7 +13,8 @@ import Layout from "../Layout/Layout";
 function App() {
   const [user, setUser] = useState(userService.getUser()); // getUser decodes our JWT token, into a javascript object
   // this object corresponds to the jwt payload which is defined in the server signup or login function that looks like
-  // this  const token = createJWT(user); // where user was the document we created from mongo
+  // this  const token = createJWT(user); 
+  // where user was the document we created from mongo
   function handleSignUpOrLogin() {
     setUser(userService.getUser()); // getting the user from localstorage decoding the jwt
   }
