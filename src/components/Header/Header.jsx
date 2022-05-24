1e5652058be9dfdbd4be65964f9fa9798897ad72
shamique202 <shamique.e@gmail.com>
2022-05-24 04:30:44 -0400

    added image source to navbar and icons

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Header, Segment, Image, Icon } from 'semantic-ui-react';
+
+export default function PageHeader({ user, handleLogout }) {
+    return ( <Segment clearing>
+            <Header as='h3' floated='right'>
+                <Link to={`/${user.username}`}><Image src={user.photoUrl ? user.photoUrl : "https://react.semantic-ui.com/images/wireframe/square-image.png"} avatar></Image></Link>
+                <Link to="/"><Icon color='grey' name="university"></Icon></Link>
+                <Link to="/feed"><Icon color='grey' name='camera retro'></Icon></Link>
+                <Link to='' color='grey' onClick={handleLogout}>Logout</Link>
+            </Header>
+            <Header as='h4' floated='left'>
+                <Link to="/"><Image src="https://i.imgur.com/12PBKfs.jpg" size="small"></Image></Link>
+            </Header>
+        </Segment>
+    );
+}
